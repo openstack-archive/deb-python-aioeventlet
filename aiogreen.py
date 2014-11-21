5306ff1fa43b21540087aaae343ae00c6d4c9a49
Victor Stinner <victor.stinner@gmail.com>
2014-11-21 02:49:27 +0100

    Only set "debug_exceptions" of the eventlet hub when the debug mode of the event loop is enabled.

--- aiogreen.py
+++ aiogreen.py
@@ -192,6 +192,8 @@ class EventLoop(asyncio.SelectorEventLoop):
     def set_debug(self, debug):
         super(EventLoop, self).set_debug(debug)
 
+        self._hub.debug_exceptions = debug
+
         # Detect blocking eventlet functions. The feature is implemented with
         # signal.alarm() which is is not available on Windows.
         self._hub.debug_blocking = debug and (sys.platform != 'win32')
